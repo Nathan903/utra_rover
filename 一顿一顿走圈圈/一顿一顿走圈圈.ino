4852de050b759a621e437bf68edfb114b815af17
Nathan <30803215+Nathan903@users.noreply.github.com>
2024-01-21 13:15:17 -0500

    左右摇头

--- 一顿一顿走圈圈/一顿一顿走圈圈.ino
+++ 一顿一顿走圈圈/一顿一顿走圈圈.ino
@@ -3,7 +3,7 @@ int TURNSPEED=77; // speed during turning
 int DIRECTION=1; // make -1 to make car go in reverse
 int THRES = 111; // threshold for determining if the sensor is seeing black
 int ERROR_THRES = 66; // threshold to determine if error (diff between left and right sensor) is large enough to warrant a turn
-float PERCENTAGE_OF_IDLE = 0.95; // percentage of the loops (not time) which the car is stopped.
+float PERCENTAGE_OF_IDLE = 0.93; // percentage of the loops (not time) which the car is stopped.
 
 ////////////// ports
 int motor1pin1 = 3;//2;
@@ -44,6 +44,7 @@ void left(int direction, int speed) {
 
 void right(int direction, int speed) {  
   // forward = 1, stop = 0, backward = -1
+  speed *=1.2;
   if (direction == 0) {// Stop
     analogWrite(motor2pin1, 0);
     analogWrite(motor2pin2, 0);
@@ -66,7 +67,9 @@ void turnright(){
 }
 #define LEFT_SEE_BLACK (absoluteValue(leftSensorValue)>THRES)
 #define RIGHT_SEE_BLACK (absoluteValue(rightSensorValue)>THRES)
-
+#define leftSensorValue analogRead(leftSensorPin)
+#define rightSensorValue analogRead(rightSensorPin)
+    
 long counter=0; // 
 const long counter_total =100;
 void loop() {
@@ -90,34 +93,43 @@ void loop() {
 // TESTTURNING
   if(counter==counter_total-1){
     int turnamount = 0;
+    {
     while(! LEFT_SEE_BLACK && !RIGHT_SEE_BLACK ){
-      int leftSensorValue = analogRead(leftSensorPin);
-      int rightSensorValue = analogRead(rightSensorPin);
-      turnamount+=2;
+      turnamount+=5;
       //try right
       for (int i = 0; i<=turnamount; i++){
+        Serial.println("TRY_RIGHT");
         turnright();
       }
       if(RIGHT_SEE_BLACK ){ 
         // keep turning
         while( RIGHT_SEE_BLACK ){ turnright(); }
         break; 
+      } else if(LEFT_SEE_BLACK ){ 
+        // keep turning
+        while( LEFT_SEE_BLACK ){ turnleft(); }
+        break; 
       }
+
       for (int i = 0; i<=2*turnamount; i++){
+        Serial.print("TRY_LEFT");
+        Serial.println(leftSensorValue);
         turnleft();
       }
       if(LEFT_SEE_BLACK){ 
-        while(LEFT_SEE_BLACK){
-          turnleft();
-        }
+        while(LEFT_SEE_BLACK){turnleft();}
+        break; 
+      }else if(RIGHT_SEE_BLACK ){ 
+        // keep turning
+        while( RIGHT_SEE_BLACK ){ turnright(); }
         break; 
       }
     }
+    }
   }
+  Serial.println("=================FIXED========================");
     ///////////////////////////////////
 
-  int leftSensorValue = analogRead(leftSensorPin);
-  int rightSensorValue = analogRead(rightSensorPin);
   int error = rightSensorValue - leftSensorValue;
   if (absoluteValue(rightSensorValue)>THRES && absoluteValue(leftSensorValue)>THRES){
     Serial.print("BACKWARD=====================================");
